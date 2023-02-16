0fc2136cabf7b8b2bb1f4873a69771b64b4cbcc5
nneeraja611 <85090026+nneeraja611@users.noreply.github.com>
2023-02-16 20:14:42 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "This is PES2UG20CS528\n";
+  cout << "This is CC assignment 2\n";
+  return 0;
+}
